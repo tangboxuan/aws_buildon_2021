5d6494a9e5755440c1ca843f6c313f125d03c39e
funkybunny99 <67521352+funkybunny99@users.noreply.github.com>
2021-08-02 12:59:21 +0800

    added navigation and side bar

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 import { API } from 'aws-amplify'
 import Predictions from '@aws-amplify/predictions';
 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './pages';
 
-import NavBar from './Navbar';
+// import NavBar from './Components/Navbar';
+// import Sidebar from './Components/Sidebar';
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -84,8 +87,12 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <NavBar/>
-        <header className="App-header">
+        <Router>
+          <Switch>
+            <Route path='/' component={Home} exact />
+          </Switch>
+        </Router>   
+        {/* <header className="App-header">
           <form onSubmit={this.getLowball}>
             <textarea
               name="lowballInput"
@@ -141,7 +148,7 @@ class App extends React.Component {
             <button type="submit">Get Retype Sentiment</button>
           </form>
           <h1>{this.state.retypeOutput}</h1>
-        </header>
+        </header> */}
       </div>
   )};
 }
