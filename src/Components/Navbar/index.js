fde06d1a8dd887454d6694a68be6818d882213a9
funkybunny99 <67521352+funkybunny99@users.noreply.github.com>
2021-08-03 21:54:16 +0800

    updated website (home + info)

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { FaBars } from 'react-icons/fa';
-// import { IconContext } from 'react-icons/lib';
+import { IconContext } from 'react-icons/lib';
 import { animateScroll as scroll } from 'react-scroll';
 import {
   MobileIcon,
@@ -16,30 +16,80 @@ import {
 
 const Navbar = ({toggle}) => {
 
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeNav);
+  }, []);
+
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+  
   return (
     <>
-      <Nav>
-        <NavbarContainer>
-        <NavLogo to='/'>ReType</NavLogo>
-        <MobileIcon onClick={toggle}>
-          <FaBars />
-        </MobileIcon>
-        <NavMenu>
-          <NavItem>
-            <NavLinks to='about'>About</NavLinks>
-          </NavItem>
-          <NavItem>
-            <NavLinks to='low-balling'>Low-balling Feature</NavLinks>
-          </NavItem>
-          <NavItem>
-            <NavLinks to='sentiment'>Sentiment Analysis</NavLinks>
-          </NavItem>
-        </NavMenu>
-        <NavBtn>
-          <NavBtnLink to='figma'>To Figma UI</NavBtnLink>
-        </NavBtn>
-      </NavbarContainer>
-      </Nav>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <Nav scrollNav={scrollNav}>
+          <NavbarContainer>
+            <NavLogo onClick={toggleHome} to='/'>
+              ReType
+            </NavLogo>
+            <MobileIcon onClick={toggle}>
+              <FaBars />
+            </MobileIcon>
+            <NavMenu>
+              <NavItem>
+                <NavLinks
+                  to='about'
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact='true'
+                  offset={-80}
+                >
+                  About
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks
+                  to='low-balling'
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact='true'
+                  offset={-80}
+                >
+                  Low-balling
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks
+                  to='sentiment'
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact='true'
+                  offset={-80}
+                >
+                  Sentiment Analysis
+                </NavLinks>
+              </NavItem>
+            </NavMenu>
+            <NavBtn>
+              <NavBtnLink to={{ pathname: "https://www.figma.com/proto/I7hPeR8rNpnsCFhkYFVqit/Carousell-Build-On-hackathon?node-id=0%3A1" }} 
+              target="_parent">Figma UI</NavBtnLink>
+            </NavBtn>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
     </>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import Navbar from '../Components/Navbar';
 import Sidebar from '../Components/Sidebar';
+import HeroSection from '../Components/HeroSection'
+import InfoSection from '../Components/InfoSection'
+import { homeObjOne , homeObjTwo, homeObjThree} from '../Components/InfoSection/Data';
 
 function Home() {
     const [isOpen, setIsOpen] = useState(false);
@@ -12,6 +15,10 @@ function Home() {
       <>
         <Sidebar isOpen={isOpen} toggle={toggle} />
         <Navbar toggle={toggle} />
+        <HeroSection/>
+        <InfoSection {...homeObjOne} />
+        <InfoSection {...homeObjTwo} />
+        <InfoSection {...homeObjThree} />
       </>
     );
   }
